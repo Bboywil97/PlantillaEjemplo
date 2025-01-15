961b9b1969ba6a1798e6895f242935a88cd0f3fd
Bboywil97 <131089907+Bboywil97@users.noreply.github.com>
2025-01-15 12:04:09 -0600

    CommitPrueba

--- Shared/Inventario.cs
+++ Shared/Inventario.cs
@@ -7,21 +7,22 @@ using System.Threading.Tasks;
 
 namespace PlantillaEjemplo.Shared
 {
-    public class Articulos
-    {
-        public int Id { get; set; }
-        public string Nombre { get; set; } = string.Empty;
-        public DateTime? FechaRegistro { get; set; } = DateTime.Now;
-        public string Marca { get; set; } = string.Empty;
-        public string Estatus { get; set; } = string.Empty;
-        public DateTime? FechaCompra { get; set; } = DateTime.Now;
-        public DateTime? FechaAdquisicion { get; set; } = DateTime.Now;
-        public double? PrecioAdquisicion { get; set; }
-        public string Producto { get; set; } = string.Empty;
-        public int? Cantidad { get; set; }
-        public string Caracteristicas { get; set; } = string.Empty;
-    }
+    
 
+}
+public class Articulos
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; } = string.Empty;
+    public DateTime? FechaRegistro { get; set; } = DateTime.Now;
+    public string Marca { get; set; } = string.Empty;
+    public string Estatus { get; set; } = string.Empty;
+    public DateTime? FechaCompra { get; set; } = DateTime.Now;
+    public DateTime? FechaAdquisicion { get; set; } = DateTime.Now;
+    public double? PrecioAdquisicion { get; set; }
+    public string Producto { get; set; } = string.Empty;
+    public int? Cantidad { get; set; }
+    public string Caracteristicas { get; set; } = string.Empty;
 }
 
 public class ArticulosService
