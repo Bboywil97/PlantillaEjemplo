4f051f008fdfbb193c7dbfc825c8eba603edc782
yzyDRNX <edaniellriv@gmail.com>
2025-01-15 13:09:10 -0600

    Controlador

---
+++ Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using PlantillaEjemplo.Shared;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace PlantillaEjemplo.Server.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+
+        public DbSet<Articulo> Articulos { get; set; }
+    }
+}
