56ecefb49216ac6fb27751c367caf23bd0a03ff3
Bboywil97 <131089907+Bboywil97@users.noreply.github.com>
2025-01-16 13:17:02 -0600

    Todos los cambios

--- Shared/Inventario.cs
+++ Shared/Inventario.cs
@@ -19,8 +19,12 @@ namespace PlantillaEjemplo.Shared
         public double? PrecioAdquisicion { get; set; }
         public string Producto { get; set; } = string.Empty;
         public int? Cantidad { get; set; }
-        public string Caracteristicas { get; set; } = string.Empty;
+        public string Descripcion { get; set; } = string.Empty;
     }
+ 
+
+}
+
 
     public class ArticulosService
     {
@@ -36,38 +40,24 @@ namespace PlantillaEjemplo.Shared
     {
         public List<Usuario> Usuarios { get; private set; } = new List<Usuario>();
 
-    public void AgregarUsuario(Usuario usuario)
-    {
-        Usuarios.Add(usuario);
+        public void AgregarUsuario(Usuario usuario)
+        {
+            Usuarios.Add(usuario);
+        }
     }
-}
 
-public class Usuario
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
-    public string RFC { get; set; } = string.Empty;
-    public bool IsEditing { get; set; } = false;
-    public string PasswordMasked => new string('*', Password.Length);
-}
-
-public class Adscripciones
-{
-    public int Id { get; set; }
-    public string Clave { get; set; } = string.Empty;
-    public string Nombre { get; set; } = string.Empty;
-    public string Direccion { get; set; } = string.Empty;
-}
-
-public class AdscripcionesService
-{
-    public List<Adscripciones> Adscripciones { get; private set; } = new List<Adscripciones>();
-
-    public void AgregarAdscripciones(Adscripciones adscripciones)
+    public class Usuario
     {
-        Adscripciones.Add(adscripciones);
+        public object Nombre;
+
+
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string RFC { get; set; } = string.Empty;
+        public bool IsEditing { get; set; } = false;
+        public string PasswordMasked => new string('*', Password.Length);
     }
-}
+
 
